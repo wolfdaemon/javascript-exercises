8c8cdd761899abf8c2801eacad8a96b89e570fda
Michael "Cal" Krug <wolfdaemon@murena.io>
2024-04-08 00:10:08 -0500

    fix: Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let string2;
+    if (num < 0) {
+        string2 = "ERROR";
+    } else {
+        string2 = string.repeat(num);
+    }
+    return string2;
 };
 
 // Do not edit below this line
