d873cc03730105977ad301532ee18a5fdd26b764
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-15 20:30:36 -0500

    feat: solutions to 01, 02, 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, number) {
+	if (number < 0) {
+		return "ERROR";
+	} else {
+		let string2 = "";
+		for (i = 0; i < number; i++) {
+			string2 += string;
+		}
+		string = string2;
+		return string;
+	}
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	let string2 = ""; // define for the scope of the function (return statement)
+	
+	for (let i = string.length - 1; i >= 0; i--) {
+		string2 += string[i];
+	}
 
+	return string2;
 };
 
 // Do not edit below this line
