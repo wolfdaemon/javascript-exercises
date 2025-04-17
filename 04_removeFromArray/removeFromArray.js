adfc34068bb99bb17c51430ebf363fbae6ac5843
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-16 22:44:15 -0500

    feat: progress on 04, success on tests 01, 02

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, num, num2 = -1) {
+	num = num - 1;
+	array.splice(num, 1);
+
+	if (num2 >= 0) {
+		num2 = num2 - 1;
+		array.splice(num2, 1);
+	}
+	
+	return array;
 };
 
 // Do not edit below this line
