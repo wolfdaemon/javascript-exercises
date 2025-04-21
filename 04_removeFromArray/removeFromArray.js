9be791f0749a3e1f90a33f377a6ab377272e54e2
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-21 01:35:38 -0500

    feat: problem 04, solutions for tests 1-3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,20 @@
 const removeFromArray = function(array, num, num2 = -1) {
-	num = num - 1;
-	array.splice(num, 1);
-
-	if (num2 >= 0) {
-	//	num2 = num2 - 1;
-	//	array.splice(num2, ...arguments);
-		array2 = array.filter((num2) => num2 != num2);
-	}
-
 	// in order to remove multiples of the same value within an array, we probably need to implement a loop somewhere.
 	// there also might need to be an implementation with regard to the spread syntax [...] that was mentioned in the README file.
 	// TODO: Look at the spread syntax and perhaps implement it into the loop.
-	//
-	//
+
 	// NOTE: I"m onto something with the filter() method. We can use filter() to "filter" out multiple indexes that don't meet a certain requirement, so we probably need to use that to fulfill "filtering" out entries of the same value. I am just not sure how exactly to implement this into a condition properly. I suspect that I need to create another variable to separate the value of the actual index versus the index of the actual value.
 	
-	return array;
+	let numCompare = num;
+	let arrayNew = array.filter((num) => num != numCompare);
+
+	if (num2 >= 0) {
+		let numCompare2 = num2;
+		let arrayNew2 = arrayNew.filter((num2) => num2 != numCompare2);
+		return arrayNew2;
+	}
+
+	return arrayNew;
 };
 
 // Do not edit below this line
