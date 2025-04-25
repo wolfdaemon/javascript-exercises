fb095828663c3aeeaea697eedc7d7b1d0495f9be
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-24 20:30:15 -0500

    feat: task 05, tests 1-3 complete, progress on 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+	let z = 0;
 
+	if (x < y) {
+		for (i = x; i <= y; i++) {
+			z += i;
+		}
+		return z;
+	} else if (x > y) {
+		for (i = y; i <= x; i++) {
+			z += i;
+		}
+		return z;
+	} else if (x < 0 || y < 0) { // if numbers negative
+		console.log("ERROR: Negative numbers");
+	} else if ( ) // if numbers non-integer {
+
+	}
 };
 
 // Do not edit below this line
