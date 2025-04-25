bc4bfab1c082a749221060cccfff422d8f55582e
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-25 00:34:33 -0500

    feat: complete tasks 05 & 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+	if (year % 4 === 0) {
+		if (year % 100 === 0 && year % 400 !== 0) {
+			return false;
+		} else {
+			return true;
+		}
+	} else {
+		return false;
+	}
+}
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = leapYears
