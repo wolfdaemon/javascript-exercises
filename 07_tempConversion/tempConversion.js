802a36702062210449310431eedf86060b4a711d
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-25 14:16:16 -0500

    feat: solution to problem 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
-};
+/*
+ * Celsius to Fahrenheit Formula: (°C * 1.8) + 32 = °F
+ * Fahrenheit to Celsius Formula: (°F - 32) / 1.8 = °C
+*/
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(tempF) {
+	tempC = (tempF - 32) / 1.8;
+	tempC = Math.round(tempC * 10)/10;
+	return tempC;
+}
+
+const convertToFahrenheit = function(tempC) {
+	tempF = (tempC * 1.8) + 32;
+	tempF = Math.round(tempF * 10)/10;
+	return tempF;
 };
 
 // Do not edit below this line
