f68a13c30f86f488a635c9084276ee271e459879
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-04-29 05:21:38 -0500

    feat: solution to test 1 within task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	stringArray = Array.from(string);
+	stringReverse = stringArray.reverse().join("");
 
+	if (string === stringReverse) {
+		return true;
+	}
 };
 
 // Do not edit below this line
