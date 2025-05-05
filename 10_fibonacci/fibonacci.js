393dd3a8f4edec5aca9bbe7f464b3305da8ddae4
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-05-04 21:17:18 -0500

    feat: solutions to tasks 09 & 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,42 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
-};
+	let arr = [ 1, 1 ];
+	let numNext = 0;
+
+	let numPrev = 1;
+	let numPrevIndex = 0;
+
+	let numPrev1 = 1;
+	let numPrev1Index = 1;
+
+	if (num < 0) {
+		return "OOPS";
+	}
+
+	if (num == 0) {
+		return numNext;
+	}
+
+	if (num == 1) {
+		arr.push(numPrev);
+		numNext = arr[0];
+		arr.push(numNext);
+		return numNext;
+	}
+
+
+	// num = 2
+	for (let i = 1; i < num; i++) {
+		numNext = arr[numPrevIndex] + arr[numPrev1Index]; // 2
+		numPrevIndex++; // 0 
+		numPrev1Index++; // 1
+		arr.push(numNext);
+	}
+	return arr[numPrevIndex];
+}
+
+// node inspect tester (DELET when done)
+fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
