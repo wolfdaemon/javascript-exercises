9d1604720395cd8b51a4323ba09d7b18d86889e2
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-05-07 19:23:23 -0500

    feat: solution to task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+	bookTitles = [];
+	for (book of books) {
+		bookTitles.push(book.title);
+	}
+	return bookTitles;
 };
 
 // Do not edit below this line
