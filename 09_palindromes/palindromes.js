7a5a851f5c6f90749ee9368c6b402722766fbde5
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-05-08 20:14:29 -0500

    fix: solution/adjustment to task 09, slight mod on task 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,40 @@
 const palindromes = function (string) {
-	stringArray = Array.from(string);
-	stringReverse = stringArray.reverse().join("");
 
-	if (string === stringReverse) {
+	let stringArray;
+	let stringReverse;
+
+	stringArray = string;
+
+
+	if (string.indexOf(",") > -1) {
+		stringArray = stringArray.split(",");
+		stringArray = stringArray.join("");
+		stringArray = stringArray.replace(/\s+/g, "");
+	}
+
+	if (string.indexOf("!") > -1) {
+		stringArray = stringArray.split("!");
+		stringArray.pop();
+		stringArray = stringArray[0];
+	}
+
+	if (string.indexOf(".") > -1) {
+		stringArray = stringArray.split(".");
+		stringArray.pop();
+		stringArray = stringArray[0];
+	}
+
+	stringArray = Array.from(stringArray);
+	stringReverse = stringArray.reverse().join("").toLowerCase();
+	stringArray = stringArray.reverse().join("").toLowerCase();
+
+	stringArray = stringArray.replace(/\s+/g, "");
+	stringReverse = stringReverse.replace(/\s+/g, "");
+
+	if (stringArray === stringReverse) {
 		return true;
+	} else {
+		return false;
 	}
 };
 
