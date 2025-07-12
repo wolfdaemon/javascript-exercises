4037fbb35b225af6764542bf5a6f4ca5121821a0
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-07-11 19:34:37 -0500

    fix: resetting problems 12-16 (unauthorized)
    Resetting the state of my solutions for problems 12-16 because I wasn't
    supposed to do them at the time I did them. Now that I'm on
    [Object Basics | Foundations](https://www.theodinproject.com/lessons/foundations-object-basics)
    , I'm going to redo them. If nothing else, it will help test/cement the
    knowledge I've learned so far. It can only help me to redo them now.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,40 +1,26 @@
-const add = function(y, z) {
-	return y + z;
+const add = function() {
+	
 };
 
-const subtract = function(y, z) {
-	return y - z;
+const subtract = function() {
+	
 };
 
-const sum = function(array) {
-	const initialValue = 0;
-	return array.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue); // why is there a comma before `initialValue`, and how come that helps return a value (& namely, help "compute the sum of an empty array")
+const sum = function() {
+	
 };
 
-const multiply = function(array) {
-	let i = 1;
-	for (const arr of array) {
-		i = i * arr;
-	}
-	return i;
+const multiply = function() {
+
 };
 
-const power = function(y, z) {
-	let x = y;
-	for (i = 1; i < z; i++) {
-		x *= y;
-	}
-	return x;
+const power = function() {
+	
 };
 
-const factorial = function(num) {
-  let i = 1;
-  while (num > 0) {
-    i *= num;
-    num--;
-  }
-  return i;
-}
+const factorial = function() {
+	
+};
 
 // Do not edit below this line
 module.exports = {
