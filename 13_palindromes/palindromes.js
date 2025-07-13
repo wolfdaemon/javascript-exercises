2c8c7f665e3ecb18f169b465149b76a7bbd78d71
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-07-12 21:05:34 -0500

    fix: removed console logs from palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,16 +1,12 @@
 const palindromes = function (text) {
 	let textSplit = text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, "").toLowerCase();
 	let textReverse = textSplit.split("").reverse().join("");
-	console.log(textSplit);
-	console.log(textReverse);
 	if (textSplit === textReverse) {
 		return true;
 	} else {
 		return false;
 	}
 };
-
-console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
