2144bd6d55cf689e830bc58e1eba09b3d04ea2e0
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-07-14 06:39:06 -0500

    feat: solution to 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	n = Number(n);
+	let seq = [0, 1];
 
-};
+	if (n < 0) {
+		return "OOPS";
+	}
+
+	if (n <= 1) {
+		return n;
+	}
+
+	for (i = 2; i <= n; i++) {
+		let n1 = seq[i - 1];
+		let n2 = seq[i - 2];
+		let nNext = n1 + n2;
+		seq.push(nNext);
+		console.log(seq + " <- seq");
+		console.log(nNext + " <- nNext");
+	}
+
+	let nPlace = n;
+	return seq[nPlace];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
