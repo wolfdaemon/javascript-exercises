dcc50cabd99e8414682b96743bc3ac0ffb2cddaa
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-07-15 18:25:01 -0500

    feat: solution to 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+	let booksTitles = books.map(book => book.title);
+	return booksTitles;
 };
 
 // Do not edit below this line
