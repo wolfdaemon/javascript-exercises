4c4ed55b4ca5a26107cf743405ea5371ca295882
Michael "Cal" Krug <wolfdaemon@murena.io>
2025-07-16 01:14:17 -0500

    feat: solution to 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+	let timeCurrentYear = new Date().getFullYear();
+
+	for (const person of people) {
+
+		let age;
+
+		if (person.yearOfDeath === undefined) {
+			age = timeCurrentYear - person.yearOfBirth;
+		} else {
+			age = person.yearOfDeath - person.yearOfBirth;
+		}
+
+		person.age = age;
+	}
+
+	people.sort((a, b) => b.age - a.age).splice(-2);
+
+	return people[0];
 };
 
 // Do not edit below this line
